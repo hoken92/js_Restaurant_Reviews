19dbfa5e16f663435643b891d6d9bfc1bec2133c
hoken92 <hkenneth92@gmail.com>
2024-09-30 19:58:24 -0700

    added in document object selectors

---
+++ script.js
@@ -0,0 +1,11 @@
+// Selectors
+
+const formEl = document.getElementById("review-form");
+const nameEl = document.getElementById("name");
+const restNameEl = document.getElementById("restaurant-name");
+const dateEl = document.getElementById("visit-date");
+const ratingEl = document.getElementById("rating");
+const reviewInfoEl = document.getElementById("review-info");
+const submitBtn = document.getElementById("submit-btn");
+
+const reviewDiv = document.getElementById("review-container");
