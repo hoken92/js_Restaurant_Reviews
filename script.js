a6b97ba7703e5f9d4753eadec65b5d42b123b028
hoken92 <hkenneth92@gmail.com>
2024-10-01 20:41:54 -0700

    review object  now creates into a div

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const reviewInfoEl = document.getElementById("review-info");
 const submitBtn = formEl.lastElementChild;
 
 // Reviews
-const reviewsContainer = document.querySelector(".review-container");
+const reviewsContainer = document.querySelector("#review-container");
 
 // Runs the functions when a user submits the form
 formEl.addEventListener("submit", function (evt) {
@@ -19,32 +19,46 @@ formEl.addEventListener("submit", function (evt) {
 
   //Runs the validate form function
   const reviewSubmission = validateForm(evt);
-  console.log(reviewSubmission);
 
-  // Creates a new div to store review information and appends to reviews container
-  const newReviewDiv = reviewsContainer.appendChild(
-    document.createElement("div")
-  );
-  newReviewDiv.classList.add("review");
+  if (reviewSubmission === false) {
+    return;
+  } else {
+    // Captures reviewer info into a variable
+    const reviewerString = `${reviewSubmission.name} | ${reviewSubmission.Restaurant_name} | Rating: ${reviewSubmission.rating} | Visit Date: ${reviewSubmission.date_visited}`;
+    const reviewString = `${reviewSubmission.review}`;
+    console.log(reviewString);
 
-  // Creates a <p> element and inserts reviewSubmission objects as a string
-  // Set the review-header class
-  // Append it to the reviewDiv
+    // Creates a new div to store review information and appends to reviews container
+    const newReviewDiv = reviewsContainer.appendChild(
+      document.createElement("div")
+    );
+    newReviewDiv.classList.add("review");
 
-  // Creates a date stamp and appends into the above <p> element
-  // Set the date-stamp class
+    // Creates a <p> element and inserts reviewSubmission objects as a string
+    // Set the review-header class
+    // Append it to the reviewDiv
+    const newReviewHeader = newReviewDiv.appendChild(
+      document.createElement("p")
+    );
+    newReviewHeader.classList.add("review-header");
+    newReviewHeader.textContent = reviewerString;
 
-  // Create another p tag to add reviewSubmission.review object and append it to the review container
-  // Set the review-text class
+    // Creates a date stamp and appends into the above <p> element
+    // Set the date-stamp class
+    const timeStamp = newReviewHeader.appendChild(
+      document.createElement("span")
+    );
+    timeStamp.classList.add("date-stamp");
+    timeStamp.textContent = new Date().toLocaleString();
 
-  // 1. Grab the p tag and set it into the div
-  // Place the object key/values into a variable into the p tag, like a temp literal
-  const str = reviewSubmission.name + reviewSubmission.Restaurant_name;
+    // Create another p tag to add reviewSubmission.review object and append it to the review container
+    // Set the review-text class
+    const newReviewInfo = newReviewDiv.appendChild(document.createElement("p"));
+    newReviewInfo.classList.add("review-text");
+    newReviewInfo.textContent = reviewString;
 
-  const ptag = doc;
-  console.log(str);
-
-  //   formEl.reset();
+    formEl.reset();
+  }
 });
 
 // cache objects into the below array
@@ -88,7 +102,7 @@ function validateForm(evt) {
   };
   //   console.log(reviewInfo);
   // Inserts the reviewInfo object inside review array
-  review.push(reviewInfo);
+  //   review.push(reviewInfo);
   return reviewInfo;
 }
 
